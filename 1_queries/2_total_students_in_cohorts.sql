d70d4c24b708f4930a267f34eb28e74704644555
Stas74 <splotnikov74@gmail.com>
2022-02-07 18:30:43 -0700

    BootcampX Queries 1 part

---
+++ 1_queries/2_total_students_in_cohorts.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(*)
+FROM students 
+WHERE cohort_id BETWEEN 1 AND 3;
+
+//////////////////////
+SELECT count(id)
+FROM students 
+WHERE cohort_id IN (1,2,3);
+/////////////////////////////
