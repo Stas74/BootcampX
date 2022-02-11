447e5fe485e2bbfd66afc24e50f6ccfb49a87570
Stas74 <splotnikov74@gmail.com>
2022-02-10 17:08:20 -0700

    Update files by using parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {  //  res.rows.forEach(user => {   
     console.log(`${row.cohort}: ${row.teacher}`);  // console.log(`${user.cohort} : ${user.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
